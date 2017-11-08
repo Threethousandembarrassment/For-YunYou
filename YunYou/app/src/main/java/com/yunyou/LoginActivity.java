7bc39c95a0441ca28c87501a7e104b69a5d734d3
Threethousandembarrassment <30492447+Threethousandembarrassment@users.noreply.github.com>
2017-11-08 14:46:52 +0800

    a3
    aaa

--- YunYou/app/src/main/java/com/yunyou/LoginActivity.java
+++ YunYou/app/src/main/java/com/yunyou/LoginActivity.java
@@ -11,36 +11,53 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.yunyou.util.HttpUtils;
-
 import java.io.IOException;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import static android.widget.Toast.makeText;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener  {
     private static final String TAG = "MainActivity";
-    private EditText mobile;
-    private EditText zone;
-    private EditText pass_word;
+    private EditText edmobile;
+    private EditText edpass_word;
     private Button login;
     private TextView mTvResult;
     private String url ="http://47.95.8.136/login";
-    private String mobile1,zone1,pass_word1;
+    private String mobile,pass_word;
     private final OkHttpClient client = new OkHttpClient();
-
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         login=(Button)findViewById(R.id.login);
-        mobile = (EditText) findViewById(R.id.mobile);
-        zone = (EditText) findViewById(R.id.zone);
-        pass_word= (EditText) findViewById(R.id.pass_word);
+        edmobile = (EditText) findViewById(R.id.mobile);
+        edpass_word= (EditText) findViewById(R.id.pass_word);
         mTvResult = (TextView) findViewById(R.id.login_tv_result);
-        initListener();
+        login.setOnClickListener(this);
+    }
+
+    public String login(String url, String json) throws IOException {
+        //把请求的内容字符串转换为json
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        String result = response.body().string();
+        return result;
+
+    }
+
+    public String bolwingJson(String mobile,String zone, String pass_word) {
+        return "{'mobile':" + mobile + "," + "'zone':"+ zone + "," + "'pass_word':"+ pass_word + "}";
     }
 
     //点击跳转到注册页面
@@ -51,10 +68,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     /**
      * 设置监听器
      */
-    private void initListener() {
+   /* private void initListener() {
        login.setOnClickListener(this);
     }
-
+*/
     /*
 单击事件监听
  */
@@ -69,10 +86,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
  登录
   */
     private void login() {
-       mobile1 = mobile.getText().toString().trim();
-       zone1 = zone.getText().toString().trim();
-        pass_word1 = pass_word.getText().toString().trim();
-        if(TextUtils.isEmpty(mobile1) || TextUtils.isEmpty(pass_word1)){
+        mobile = edmobile.getText().toString().trim();
+        pass_word = edpass_word.getText().toString().trim();
+        if(TextUtils.isEmpty(mobile) || TextUtils.isEmpty(pass_word)){
             makeText(LoginActivity.this, "用户名或者密码不能为空", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -80,12 +96,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         new Thread() {
             @Override
             public void run() {
-                HttpUtils httpUtils = new HttpUtils();
                 //转换为JSON
-                String user = httpUtils.bolwingJson(mobile1,zone1, pass_word1);
+                String user = bolwingJson(mobile,"86", pass_word);
                 Log.d(TAG, "user:" + user);
                 try {
-                    final String result = httpUtils.login(url, user);
+                    final String result = login(url, user);
                     Log.d(TAG, "结果:" + result);
                     //更新UI,在UI线程中
                     runOnUiThread(new Runnable() {
